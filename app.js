4b249230e147527a0a3e7ddcda3d30bbc75bca43
s96103092003 <s96103092003@gmail.com>
2018-07-28 03:46:19 +0800

    12
    3123

--- app.js
+++ app.js
@@ -183,6 +183,7 @@ function ConvertToTable(data, callBack) {
     var rows = new Array();
     var buf = new Buffer(data, 'binary');
     var str = iconv.decode(buf, 'utf-8');
+    console.log(str)
     rows = str.split("\r\n");
     for (var i = 0; i < rows.length; i++) {
         table.push(rows[i].split(","));
