b73df4f72dddab71a36e41b37ce95bbd5b59ac1c
s96103092003 <33406011+s96103092003@users.noreply.github.com>
2020-06-20 15:49:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -79,9 +79,9 @@ app.post("/", function (req, res) {
             // 判斷訊息是屬於 message 還是 postback
             // 判斷訊息是屬於 message 還是 postback」意思是發送者是否是透過機器人提供的選擇做回覆，如果「是」就是 postback；「不是」就屬於 message
             if (webhook_event.message) {
-                ReplyMessage(sender_psid, webhook_event.message);
+                let reply = ReplyMessage(sender_psid, webhook_event.message, function(flag){});
             } else if (webhook_event.postback) {
-                PostBackToMessage(sender_psid, webhook_event.postback);
+                let postback =  PostBackToMessage(sender_psid, webhook_event.postback, function(flag){});
             }
 
         });
