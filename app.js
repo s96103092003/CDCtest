8c9fe1bfd42bff1520d0eb9335ba66f23533f783
s96103092003 <s96103092003@gmail.com>
2021-05-03 16:33:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.post("/", function (req, res) {
 
     }
     if (userInput != "") {
-        request.get(config.localUrl + "/" + userInput, function (error, response, body) {
+        request.get(config.localUrl + "/" + encodeURI(userInput), function (error, response, body) {
             console.error('error:', error); // Print the error if one occurred
             console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
             console.log('body:', body); // Print the HTML for the Google homepage.
